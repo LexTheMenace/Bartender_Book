09d22394950c7b446fd3834bb06242d77fd8d5de
LexTheMenace <lexthemenace@gmail.com>
2020-08-15 10:45:32 -0500

    hashrouter

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import AgeVer from './components/AgeVer';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 import Mix from './components/Mix';
 import Saved from './components/Saved';
 function App() {
