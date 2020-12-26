49b26be9ae1da080ef7768436c23b5c66db11e31
LexTheMenace <lexthemenace@gmail.com>
2020-12-26 09:54:11 -0500

    merging branch

--- client/src/components/Drink.js
+++ client/src/components/Drink.js
@@ -1,89 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import axios from 'axios';
-import { Consumer } from '../context';
-const Drink = (props) => {
-    const { drink } = props;
-    const { name, thumbnail, ingredients, glass, instructions } = drink
-    const saveBtn = {
-        padding: '20px',
-        backgroundImage: 'linear-gradient(black, red)',
-        color: 'white',
-        zIndex: '2',
-        borderRadius: '50px',
-        fontSize: '24px',
-        border: 'none'
-    }
-    const onClick = (e) => {
-        e.preventDefault();
-        const saveBtn = document.getElementById('save');
-        const icon = saveBtn.innerHTML;
+import './Drink.css'
+const Drink = ({drink}) => {
+    const [seeMore, setSeeMore] = useState(false)
+        const onClick = (e) => {
+            e.preventDefault();
+            const saveBtn = document.getElementById('save');
+            const icon = saveBtn.innerHTML;
 
-        axios.post('/api/drinks', drink)
-        .then(res => {
-            saveBtn.innerHTML = 'Saved!'
-            setTimeout(function() {
-                saveBtn.innerHTML = icon
-              }, 2000)
-        })
-        .catch(err => console.log(err))
-    }
+            axios.post('/api/drinks', drink)
+            .then(res => { 
+              console.log(res);
+                saveBtn.innerHTML = 'Saved!'
+                setTimeout(function() {
+                    saveBtn.innerHTML = icon
+                  }, 2000)
+            })
+            .catch(err => console.log(err))
+        }
 
-    return (
-        <div className='drinkCard' >
-            <div>
-                <h2>{name}</h2>
-                <img style={{ width: '200px', marginTop: '10px', marginBottom: '10px', border: '1px solid white' }} src={thumbnail} alt={name} />
+        const { name, thumbnail, ingredients, glass, instructions } = drink
 
-                <p><strong>Glass:</strong> {glass}</p>
-
-                <button id='save' style={saveBtn} onClick={onClick} > <i class="fas fa-heart"></i> </button>
-            </div>
-            <div style={{ color: 'white' }}>
-                <h3>Ingredients</h3>
-                {ingredients.map(item => {
-                    if (!item.amt) {
-                        return <div>{item.item}</div>
-                    } else {
-                        return <div>{item.item} : {item.amt}</div>
-                    }
-                })}
-                <p>Directions: {instructions}</p>
-
-            </div>
-        </div>
-    )
-}
-export default Drink;
-/*export default class Drink extends Component {
-        
-
-
-        const { name, thumbnail, ingredients, glass, instructions } = this.state.drink
+        if (!ingredients) {
+            return (
+                <h1>Mixing...</h1>
+            )
+        }
         return (
-            <div className='drinkCard' >
-                <div>
-                    <h2>{name}</h2>
-                    <img style={{ width: '200px', marginTop: '10px', marginBottom: '10px', border: '1px solid white' }} src={thumbnail} alt={name} />
-
+            <div className='drink' >
+                <div style={{  overflow: seeMore && 'scroll'}} onClick={() => setSeeMore(!seeMore)} className={seeMore ? 'drink__card d-none' : 'drink__card'}>
+                {!seeMore ? (
+                  <>
+                 <button id='save' onClick={onClick}> <i className="fas fa-heart"></i> </button>
+                    <img style={{ border: '1px solid white' }} src={thumbnail} alt={name} />
+                    <h4 onClick={() => setSeeMore(!seeMore)}>{name}</h4>
+                    </>
+                    )
+               :     <> <h5>Ingredients:</h5>
+               <div className='drink__ingredients'>
+                   {ingredients.map(item => { 
+                       return <div style={{ display: 'flex', flexDirection: 'column', margin: '2px' }}><span style={{ backgroundColor: 'black', color: 'white' }}>{ item.item }</span> </div>
+                   })}    
+               </div>
+              <div className='drink__card__info'>
+                   <p>Directions: {instructions}</p>
+                   <p><strong>Glass:</strong> {glass}</p>
+                   </div>
+</>
+                }</div>
+                 {/* //use modal instead */}
+              {/*  {seeMore &&   <div  onClick={() => setSeeMore(!seeMore)}  style={{ overflow: 'scroll'}} className='drink__card'>
+                 <h5>Ingredients:</h5>
+                <div className='drink__ingredients'>
+                    {ingredients.map(item => { 
+                        return <div style={{ display: 'flex', flexDirection: 'column', margin: '2px' }}><span style={{ backgroundColor: 'black', color: 'white' }}>{ item.item }</span> </div>
+                    })}    
+                </div>
+                <div className='drink__card__info'>
+                    <p>Directions: {instructions}</p>
                     <p><strong>Glass:</strong> {glass}</p>
 
-                   <button id='save' style={saveBtn}onClick={this.onClick}> <i class="fas fa-heart"></i> </button>
-                 </div>
-                <div style={{color: 'white'}}>
-                    <h3>Ingredients</h3>
-                    {ingredients.map(item => {
-                        if (!item.amt){
-                                return <div>{item.item}</div>
-                            } else {
-                            return <div>{item.item} : {item.amt}</div>
-                        }
-                    })}
-                    <p>Directions: {instructions}</p>
-
+                    </div>
                 </div>
-            </div>
+                }         */} 
+            </div> 
         );
-    }
-
+    
 };
-*/
+export default Drink;
